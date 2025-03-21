25a13e85ff8b45343d47f03ad7c765b8b549d1e2
Guanya Peng <guanya.peng24@gmail.com>
2025-03-21 10:27:39 +0100

    Create nodes.py

---
+++ src/agents/research_assistant/research_assistant_agent/nodes.py
@@ -0,0 +1,235 @@
+from research_assistant_agent.schema import (
+    GenerateAnalystsState,
+    ResearchGraphState,
+    InterviewState,
+    Perspectives,
+    SearchQuery,
+)
+from research_assistant_agent.llm import llm
+
+from langchain_core.messages import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage,
+    get_buffer_string,
+)
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.document_loaders import WikipediaLoader
+from langgraph.constants import Send
+
+
+analyst_instructions = """You are tasked with creating a set of AI analyst personas. Follow these instructions carefully:
+
+1. First, review the research topic:
+{topic}
+        
+2. Examine any editorial feedback that has been optionally provided to guide creation of the analysts: 
+        
+{human_analyst_feedback}
+    
+3. Determine the most interesting themes based upon documents and / or feedback above.
+                    
+4. Pick the top {max_analysts} themes.
+
+5. Assign one analyst to each theme."""
+
+
+def create_analysts(state: GenerateAnalystsState):
+    """Create analysts"""
+
+    topic = state["topic"]
+    max_analysts = state["max_analysts"]
+    human_analyst_feedback = state.get("human_analyst_feedback", "")
+
+    # Enforce structured output
+    structured_llm = llm.with_structured_output(Perspectives)
+
+    # System message
+    system_message = analyst_instructions.format(
+        topic=topic,
+        human_analyst_feedback=human_analyst_feedback,
+        max_analysts=max_analysts,
+    )
+
+    # Generate question
+    analysts = structured_llm.invoke(
+        [SystemMessage(content=system_message)]
+        + [HumanMessage(content="Generate the set of analysts.")]
+    )
+
+    # Write the list of analysis to state
+    return {"analysts": analysts.analysts}
+
+
+def human_feedback(state: GenerateAnalystsState):
+    """No-op node that should be interrupted on"""
+    pass
+
+
+# Generate analyst question
+question_instructions = """You are an analyst tasked with interviewing an expert to learn about a specific topic. 
+
+Your goal is boil down to interesting and specific insights related to your topic.
+
+1. Interesting: Insights that people will find surprising or non-obvious.
+        
+2. Specific: Insights that avoid generalities and include specific examples from the expert.
+
+Here is your topic of focus and set of goals: {goals}
+        
+Begin by introducing yourself using a name that fits your persona, and then ask your question.
+
+Continue to ask questions to drill down and refine your understanding of the topic.
+        
+When you are satisfied with your understanding, complete the interview with: "Thank you so much for your help!"
+
+Remember to stay in character throughout your response, reflecting the persona and goals provided to you."""
+
+
+def generate_question(state: InterviewState):
+    """Node to generate a question"""
+
+    # Get state
+    analyst = state["analyst"]
+    messages = state["messages"]
+
+    # Generate question
+    system_message = question_instructions.format(goals=analyst.persona)
+    question = llm.invoke([SystemMessage(content=system_message)] + messages)
+
+    # Write messages to state
+    return {"messages": [question]}
+
+
+# Search query writing
+search_instructions = SystemMessage(
+    content=f"""You will be given a conversation between an analyst and an expert. 
+
+Your goal is to generate a well-structured query for use in retrieval and / or web-search related to the conversation.
+        
+First, analyze the full conversation.
+
+Pay particular attention to the final question posed by the analyst.
+
+Convert this final question into a well-structured web search query"""
+)
+
+
+def search_web(state: InterviewState):
+    """Retrieve docs from web search"""
+
+    # Search
+    tavily_search = TavilySearchResults(max_results=3)
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions] + state["messages"])
+
+    # Search
+    search_docs = tavily_search.invoke(search_query.search_query)
+
+    # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document href="{doc["url"]}"/>\n{doc["content"]}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+
+def search_wikipedia(state: InterviewState):
+    """Retrieve docs from wikipedia"""
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions] + state["messages"])
+
+    # Search
+    search_docs = WikipediaLoader(
+        query=search_query.search_query, load_max_docs=2
+    ).load()
+
+    # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]}
+
+
+# Generate expert answer
+answer_instructions = """You are an expert being interviewed by an analyst.
+
+Here is analyst area of focus: {goals}. 
+        
+You goal is to answer a question posed by the interviewer.
+
+To answer question, use this context:
+        
+{context}
+
+When answering questions, follow these guidelines:
+        
+1. Use only the information provided in the context. 
+        
+2. Do not introduce external information or make assumptions beyond what is explicitly stated in the context.
+
+3. The context contain sources at the topic of each individual document.
+
+4. Include these sources your answer next to any relevant statements. For example, for source # 1 use [1]. 
+
+5. List your sources in order at the bottom of your answer. [1] Source 1, [2] Source 2, etc
+        
+6. If the source is: <Document source="assistant/docs/llama3_1.pdf" page="7"/>' then just list: 
+        
+[1] assistant/docs/llama3_1.pdf, page 7 
+        
+And skip the addition of the brackets as well as the Document source preamble in your citation."""
+
+
+def generate_answer(state: InterviewState):
+    """Node to answer a question"""
+
+    # Get state
+    analyst = state["analyst"]
+    messages = state["messages"]
+    context = state["context"]
+
+    # Answer question
+    system_message = answer_instructions.format(goals=analyst.persona, context=context)
+    answer = llm.invoke([SystemMessage(content=system_message)] + messages)
+
+    # Name the message as coming from the expert
+    answer.name = "expert"
+
+    # Append it to state
+    return {"messages": [answer]}
+
+
+def save_interview(state: InterviewState):
+    """Save interviews"""
+
+    # Get messages
+    messages = state["messages"]
+
+    # Convert interview to a string
+    interview = get_buffer_string(messages)
+
+    # Save to interviews key
+    return {"interview": interview}
+
+
+def route_messages(state: InterviewState, name: str = "expert"):
+    """Route between question and answer"""
+
+    # Get messages
+    messages = state["messages"]
+    max_num_turns = state.get("max_num_turns", 2)
+
+    # Check the number of expert answers
+    num_responses = len(
