0b34eed6b7d7c0041617c2250a8d2be57b18ac59
Guanya Peng <guanya.peng24@gmail.com>
2025-05-19 20:25:30 +0200

    Refactor graph.py

--- src/agents/research_assistant/research_assistant_agent/graph.py
+++ src/agents/research_assistant/research_assistant_agent/graph.py
@@ -32,3 +32,38 @@ interview_builder.add_node("answer_question", generate_answer)
 interview_builder.add_node("save_interview", save_interview)
 interview_builder.add_node("write_section", write_section)
 
+# Flow
+interview_builder.add_edge(START, "ask_question")
+interview_builder.add_edge("ask_question", "search_web")
+interview_builder.add_edge("ask_question", "search_wikipedia")
+interview_builder.add_edge("search_web", "answer_question")
+interview_builder.add_edge("search_wikipedia", "answer_question")
+interview_builder.add_conditional_edges("answer_question", route_messages,['ask_question','save_interview'])
+interview_builder.add_edge("save_interview", "write_section")
+interview_builder.add_edge("write_section", END)
+
+
+## Research Graph
+
+# Add nodes and edges 
+builder = StateGraph(ResearchGraphState)
+builder.add_node("create_analysts", create_analysts)
+builder.add_node("human_feedback", human_feedback)
+builder.add_node("conduct_interview", interview_builder.compile())
+builder.add_node("write_report",write_report)
+builder.add_node("write_introduction",write_introduction)
+builder.add_node("write_conclusion",write_conclusion)
+builder.add_node("finalize_report",finalize_report)
+
+# Logic
+builder.add_edge(START, "create_analysts")
+builder.add_edge("create_analysts", "human_feedback")
+builder.add_conditional_edges("human_feedback", initiate_all_interviews, ["create_analysts", "conduct_interview"])
+builder.add_edge("conduct_interview", "write_report")
+builder.add_edge("conduct_interview", "write_introduction")
+builder.add_edge("conduct_interview", "write_conclusion")
+builder.add_edge(["write_conclusion", "write_report", "write_introduction"], "finalize_report")
+builder.add_edge("finalize_report", END)
+
+# Compile
+graph = builder.compile(interrupt_before=['human_feedback'])
