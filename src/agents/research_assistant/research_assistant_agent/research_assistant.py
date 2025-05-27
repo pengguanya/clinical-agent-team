5c12eb02d2d2681a2202a3d0fb5f2170d13db729
Guanya Peng <guanya.peng24@gmail.com>
2025-05-27 07:52:21 +0200

    Work on research_assistant.py

--- src/agents/research_assistant/research_assistant_agent/research_assistant.py
+++ src/agents/research_assistant/research_assistant_agent/research_assistant.py
@@ -107,3 +107,112 @@ def create_analysts(state: GenerateAnalystsState):
 
 def human_feedback(state: GenerateAnalystsState):
     """ No-op node that should be interrupted on """
+    pass
+
+# Generate analyst question
+question_instructions = """You are an analyst tasked with interviewing an expert to learn about a specific topic. 
+
+Your goal is boil down to interesting and specific insights related to your topic.
+
+1. Interesting: Insights that people will find surprising or non-obvious.
+        
+2. Specific: Insights that avoid generalities and include specific examples from the expert.
+
+Here is your topic of focus and set of goals: {goals}
+        
+Begin by introducing yourself using a name that fits your persona, and then ask your question.
+
+Continue to ask questions to drill down and refine your understanding of the topic.
+        
+When you are satisfied with your understanding, complete the interview with: "Thank you so much for your help!"
+
+Remember to stay in character throughout your response, reflecting the persona and goals provided to you."""
+
+def generate_question(state: InterviewState):
+
+    """ Node to generate a question """
+
+    # Get state
+    analyst = state["analyst"]
+    messages = state["messages"]
+
+    # Generate question 
+    system_message = question_instructions.format(goals=analyst.persona)
+    question = llm.invoke([SystemMessage(content=system_message)]+messages)
+        
+    # Write messages to state
+    return {"messages": [question]}
+
+# Search query writing
+search_instructions = SystemMessage(content=f"""You will be given a conversation between an analyst and an expert. 
+
+Your goal is to generate a well-structured query for use in retrieval and / or web-search related to the conversation.
+        
+First, analyze the full conversation.
+
+Pay particular attention to the final question posed by the analyst.
+
+Convert this final question into a well-structured web search query""")
+
+def search_web(state: InterviewState):
+    
+    """ Retrieve docs from web search """
+
+    # Search
+    tavily_search = TavilySearchResults(max_results=3)
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions]+state['messages'])
+    
+    # Search
+    search_docs = tavily_search.invoke(search_query.search_query)
+
+     # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document href="{doc["url"]}"/>\n{doc["content"]}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]} 
+
+def search_wikipedia(state: InterviewState):
+    
+    """ Retrieve docs from wikipedia """
+
+    # Search query
+    structured_llm = llm.with_structured_output(SearchQuery)
+    search_query = structured_llm.invoke([search_instructions]+state['messages'])
+    
+    # Search
+    search_docs = WikipediaLoader(query=search_query.search_query, 
+                                  load_max_docs=2).load()
+
+     # Format
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+
+    return {"context": [formatted_search_docs]} 
+
+# Generate expert answer
+answer_instructions = """You are an expert being interviewed by an analyst.
+
+Here is analyst area of focus: {goals}. 
+        
+You goal is to answer a question posed by the interviewer.
+
+To answer question, use this context:
+        
+{context}
+
+When answering questions, follow these guidelines:
+        
+1. Use only the information provided in the context. 
+        
+2. Do not introduce external information or make assumptions beyond what is explicitly stated in the context.
